ba26f9abb6333b4b500b81c9a08237a44cb3d92c
abuabdirohman4 <abuabdirohman4@gmail.com>
2024-08-10 20:42:04 +0700

    feat : update type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-export type TimeLog = PrismaClient["timeLog"];
-export type User = PrismaClient["user"];
-
-export interface BulletPoint {
-  id?: number;
-  text: string;
-  indent: number;
-  order: number;
+export interface ReactSelect {
+  value: number | string;
+  label: string;
 }
 
 export interface Client {
@@ -35,32 +29,19 @@ export interface Period {
   Week?: Week[];
 }
 
-export interface VisionCategory {
-  id?: number;
-  name: string;
-  Vision?: Vision[];
-}
-
-export interface Vision {
-  id?: number;
-  Client?: Client;
-  clientId?: number;
-  name?: string;
-  category: number;
-  startDate: Date;
-  endDate: Date;
-  VisionCategory?: VisionCategory;
-}
-
 export interface HighFocusGoal {
   id?: number;
   Client?: Client;
   clientId?: number;
   name: string;
   motivation?: string;
-  Task?: Task[];
-  SelfDevelopmentCurriculum?: SelfDevelopmentCurriculum[];
-  StatusHighFocusGoal?: StatusHighFocusGoal[];
+  periodName: string;
+  point: number;
+  order: number;
+  completed: boolean;
+  // Task?: Task[];
+  // SelfDevelopmentCurriculum?: SelfDevelopmentCurriculum[];
+  // StatusHighFocusGoal?: StatusHighFocusGoal[];
 }
 
 export interface StatusHighFocusGoal {
@@ -70,26 +51,8 @@ export interface StatusHighFocusGoal {
   Period?: Period;
   periodName: string;
   point: number;
-  priority: number;
-}
-
-export interface SelfDevelopmentCurriculum {
-  id?: number;
-  Client?: Client;
-  clientId?: number;
-  skill: string;
   order: number;
-  highFocusGoalId: number;
-  HighFocusGoal?: HighFocusGoal;
-  Knowledge?: Knowledge[];
-}
-
-export interface Knowledge {
-  id?: number;
-  name: string;
-  type: "Book" | "Workshop"; // Enum values
-  SelfDevelopmentCurriculum?: SelfDevelopmentCurriculum;
-  SelfDevelopmentCurriculumId: number;
+  completed: boolean;
 }
 
 export interface Task {
@@ -109,63 +72,4 @@ export interface Task {
   TaskWeek?: TaskWeek[];
   createdAt?: Date;
   updatedAt?: Date;
-}
-
-export interface TimeLog {
-  id?: number;
-  task?: Task;
-  taskId: number;
-  journal?: string;
-  startTime: Date;
-  endTime?: Date;
-  duration?: Date;
-}
-
-export interface ToDontList {
-  id?: number;
-  Client?: Client;
-  clientId?: number;
-  name: string;
-  order?: number;
-  Week?: Week;
-  weekId: number;
-}
-
-export interface BrainDump {
-  id?: number;
-  Client?: Client;
-  clientId?: number;
-  text: string;
-  day: Date;
-}
-
-export interface Day {
-  id?: number;
-  taskId: number;
-  Task?: Task;
-  date: Date;
-}
-
-export interface Week {
-  id?: number;
-  Period?: Period;
-  periodName: string;
-  week: number;
-  startDate: Date;
-  endDate: Date;
-  TaskWeeks?: TaskWeek[];
-  ToDontList?: ToDontList[];
-}
-
-export interface TaskWeek {
-  id?: number;
-  taskId: number;
-  Task?: Task;
-  weekId: number;
-  Week?: Week;
-}
-
-export interface ReactSelect {
-  value: number | string;
-  label: string;
 }
