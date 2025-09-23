2da300aebdc49488dccc84dfebb1ffdb56cc1b97
Abu Abdirohman <abuabdirohman4@gmail.com>
2025-09-23 20:47:51 +0700

    fix: adjust margin in TaskItemSkeleton for improved layout
    - Updated the margin of the item container in TaskItemSkeleton to enhance spacing and layout consistency.

--- src/components/ui/skeleton/TaskItemSkeleton.tsx
+++ src/components/ui/skeleton/TaskItemSkeleton.tsx
@@ -12,7 +12,7 @@ const TaskItemSkeleton: React.FC<TaskItemSkeletonProps> = ({
 }) => {
   return (
     <div className="flex items-center justify-between bg-white dark:bg-gray-900 rounded-lg mb-3 pl-2 pr-4 py-2 shadow-sm border border-gray-200 dark:border-gray-700">
-      <div className='flex gap-2 w-full items-center'>
+      <div className='flex gap-2 w-full items-center mr-2'>
         {orderNumber && (
           <span className="font-medium text-lg w-6 text-center select-none">
             {orderNumber}.
